dd0d8d2962b3d96f628126f98ca01f5cf309f00b
Nathan Gamble <neg6@gold20.cs.calvin.edu>
2018-09-27 09:28:53 -0400

    added a spinner for operations

--- Homework1/app/src/main/java/neg6/cs262/calvin/edu/homework1/MainActivity.java
+++ Homework1/app/src/main/java/neg6/cs262/calvin/edu/homework1/MainActivity.java
@@ -2,6 +2,8 @@ package neg6.cs262.calvin.edu.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner op_spinner = (Spinner) findViewById(R.id.op_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.operations_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        op_spinner.setAdapter(adapter);
     }
 }
