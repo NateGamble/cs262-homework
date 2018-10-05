5d263d6f6f2bea0ae2bf9ec3c50bc549661d15bf
Nathan Gamble <neg6@gold22.cs.calvin.edu>
2018-10-04 21:53:25 -0400

    Finished Lab05 Task 2.3

---
+++ Lab05/app/src/main/java/neg6/cs262/calvin/edu/lab05/FetchBook.java
@@ -0,0 +1,24 @@
+package neg6.cs262.calvin.edu.lab05;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class FetchBook extends AsyncTask<String,Void,String> {
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
+        this.mTitleText = mTitleText;
+        this.mAuthorText = mAuthorText;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
--- Lab05/app/src/main/java/neg6/cs262/calvin/edu/lab05/MainActivity.java
+++ Lab05/app/src/main/java/neg6/cs262/calvin/edu/lab05/MainActivity.java
@@ -3,15 +3,25 @@ package neg6.cs262.calvin.edu.lab05;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mBookInput;
+    private TextView mAuthorText;
+    private TextView mTitleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBookInput = (EditText) findViewById(R.id.bookInput);
+        mAuthorText = (TextView) findViewById(R.id.authorText);
+        mTitleText = (TextView) findViewById(R.id.titleText);
     }
 
     public void searchBooks(View view) {
+        String queryString = mBookInput.getText().toString();
     }
 }
