8d8c81ef2ede1467a3fe49c83b4c4151e403c2c4
Nathan Gamble <neg6@gold11.cs.calvin.edu>
2018-10-05 14:25:50 -0400

    Finished Lab05

--- Lab05/app/src/main/java/neg6/cs262/calvin/edu/lab05/MainActivity.java
+++ Lab05/app/src/main/java/neg6/cs262/calvin/edu/lab05/MainActivity.java
@@ -3,6 +3,10 @@ package neg6.cs262.calvin.edu.lab05;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +14,11 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String>{
 
     private EditText mBookInput;
     private TextView mAuthorText;
@@ -23,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         mBookInput = (EditText) findViewById(R.id.bookInput);
         mAuthorText = (TextView) findViewById(R.id.authorText);
         mTitleText = (TextView) findViewById(R.id.titleText);
+        if(getSupportLoaderManager().getLoader(0)!=null){
+            getSupportLoaderManager().initLoader(0,null,this);
+        }
     }
 
     public void searchBooks(View view) {
@@ -38,7 +49,10 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected() && queryString.length()!=0) {
-            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+//            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle,this);
             mAuthorText.setText("");
             mTitleText.setText(R.string.loading);
         }
@@ -52,5 +66,53 @@ public class MainActivity extends AppCompatActivity {
                 mTitleText.setText(R.string.no_network_connection);
             }
         }
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new BookLoader(this, bundle.getString("queryString"));
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String s) {
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            //Iterate through the results
+            for(int i = 0; i<itemsArray.length(); i++){
+                JSONObject book = itemsArray.getJSONObject(i); //Get the current item
+                String title=null;
+                String authors=null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                //If both a title and author exist, update the TextViews and return
+                if (title != null && authors != null){
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                    return;
+                }
+            }
+
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+        } catch (Exception e) {
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
     }
 }
